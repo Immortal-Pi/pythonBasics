e369b3745e8ae85a64e59f1acf1772c8ee92123a
unknown <26.amruth@gmail.com>
2023-10-08 14:06:07 +0530

    leap year logic bug

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 # import the py file to call the functions from that file
 import helloFunctionsInput
+import leapYear
 
 if __name__ == '__main__':
     # print the return value from the function
     """
       multiple line comment
     """
-    print(' hello, ' + helloFunctionsInput.func())
+    # print(' hello, ' + helloFunctionsInput.func())
+    year = input('enter year: ')
+
+    print((leapYear.leapYear(int(year))))
