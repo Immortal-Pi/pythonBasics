b7f63b638c4b6d71cdaf4d760ba6447df6037e0f
unknown <26.amruth@gmail.com>
2023-10-08 13:38:17 +0530

    update

--- helloFunctionsInput.py
+++ helloFunctionsInput.py
@@ -1,10 +1,10 @@
-#function func which can be called anywhere
+# function func which can be called anywhere
 def func():
-    #define variable
-    a=30
-    b=20
-    c=50
-    name=input('whats your name? ')
+    # define variable
+    a = 30
+    b = 20
+    c = 50
+    name = input('whats your name? ')
     print("{} and {} and {}".format(a, b, c))
-    #retun value to the main function
+    # returns value to the main function
     return name
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-#import the py file to call the functions from that file
+# import the py file to call the functions from that file
 import helloFunctionsInput
 
 if __name__ == '__main__':
- print(helloFunctionsInput.func())
+    # print the return value from the function
+    """
+      multiple line comment
+    """
+    print(' hello, ' + helloFunctionsInput.func())
