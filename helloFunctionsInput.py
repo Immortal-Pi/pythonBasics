c93baec96d3d0ddc821eb83def256b0438dcb05f
unknown <26.amruth@gmail.com>
2023-10-08 12:59:45 +0530

    added my project

---
+++ helloFunctionsInput.py
@@ -0,0 +1,10 @@
+#function func which can be called anywhere
+def func():
+    #define variable
+    a=30
+    b=20
+    c=50
+    name=input('whats your name? ')
+    print("{} and {} and {}".format(a, b, c))
+    #retun value to the main function
+    return name
---
+++ main.py
@@ -0,0 +1,5 @@
+#import the py file to call the functions from that file
+import helloFunctionsInput
+
+if __name__ == '__main__':
+ print(helloFunctionsInput.func())
