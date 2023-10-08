47aa3863000d216a888b979723251fe1d9dc8ee7
unknown <26.amruth@gmail.com>
2023-10-08 15:21:47 +0530

    type conversion update

--- typeConversion.py
+++ typeConversion.py
@@ -1,6 +1,18 @@
 def stringManipulation(inputString):
 
     #Remove whitespace from string
-    inputString = inputString.strip()
+    # inputString = inputString.strip()
+    # print ('hello, ' + inputString)
+
+    # Capitalize user's first name
+    # inputString = inputString.capitalize()
+    # print('hello, ' + inputString)
+
+    # Title Capitalize first letter of all the words
+    # inputString = inputString.title()
+    # print('hello, ' + inputString)
+
+    # Chain the commands together
+    inputString = inputString.strip().title()
     print ('hello, ' + inputString)
     return
