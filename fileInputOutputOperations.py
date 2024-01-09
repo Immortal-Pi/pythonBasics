2e42849143d0e1b32f5854674cb3f0f7d259d49a
unknown <26.amruth@gmail.com>
2024-01-09 22:55:01 +0530

    Clean untracked files and update .gitignore

--- fileInputOutputOperations.py
+++ fileInputOutputOperations.py
@@ -19,6 +19,7 @@ def fileiooperations():
     with open("names.txt","r") as file:
         lines=file.readlines()
     for line in lines:
-        print(line)
+        print(line + 'Hello')
+
 
     return
