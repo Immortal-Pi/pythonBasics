75c8d87c3aaa98932f4f441f23ea684ec6cfa55a
unknown <26.amruth@gmail.com>
2024-01-10 11:16:42 +0530

    file io

--- fileInputOutputOperations.py
+++ fileInputOutputOperations.py
@@ -9,17 +9,23 @@ def fileiooperations():
 
 
     #syntax two
-    # with open("names.txt","a") as file:
-    #     for name in sorted(names):
-    #     # print(f"Hello,{name} ")
-    #         file.write(f'{name}\n') #write operation
+    with open("names.txt","a") as file:
+        for name in sorted(names):
+        # print(f"Hello,{name} ")
+            file.write(f'{name}\n') #write operation
     # file.close() needed for syntax one
 
-
+    # syntax 1 read
+    #read all the lines at once
     with open("names.txt","r") as file:
         lines=file.readlines()
-    for line in lines:
-        print(line + 'Hello')
+    for line in sorted(lines):
+        print(line.rstrip())
 
 
+
+    # syntax 2 read
+    # with open("names.txt","r") as file:
+    #     for line in file:
+    #         print(line.rstrip())
     return
