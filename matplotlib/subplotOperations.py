c152e0e83a2a746225ea134d02397d39f86eff25
unknown <26.amruth@gmail.com>
2024-01-26 16:59:49 +0530

    help

--- matplotlib/subplotOperations.py
+++ matplotlib/subplotOperations.py
@@ -2,8 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-x=np.arange(0,100,1)
+x=np.arange(0,30,0.2)
 y1=np.sin(x)
+y3=np.tan(x)
 y2=x**2 + 2*x
 
 #plt.subplot(no of rows, no of columns,indexed position in wich to be placed)
@@ -13,7 +14,7 @@ ax3=plt.subplot(224)
 
 ax1.plot(x,y1,'r')
 ax2.plot(x,y2,'g')
-ax3.plot(x,y1,'b')
+ax3.plot(x,y3,'b')
 
 #graphs not interecting each other
 plt.tight_layout()
